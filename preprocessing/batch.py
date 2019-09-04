e7d1c2bef3daea3b6ce3671fa6ae220dea17b4c4
Martin Westerkamp <westerkamp@tu-berlin.de>
2019-09-04 11:22:35 +0200

    add script for calculating multiple batches

---
+++ preprocessing/batch.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+import os
+from create_input import generateZokratesInputFromBlock
+
+cmd_compute_witness = 'zokrates compute-witness --light -a '
+cmd_generate_proof = 'zokrates generate-proof'
+
+
+for i in range(20,21):
+    #file = open('input','w+')
+    #file.write(generateZokratesInputFromBlock((i-1)*504+1, 504))
+    #file.close()
+    #os.system(cmd_compute_witness)
+    os.system(cmd_compute_witness + generateZokratesInputFromBlock((i-1)*504+1, 504))
+    os.system(cmd_generate_proof)
+    os.system('mv witness output/witness' + str(i))
+    os.system('mv proof.json output/proof' + str(i) + '.json')
