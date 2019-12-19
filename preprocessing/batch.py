40169c94d73085c92c00bf29e7f2a2f5e094c3b0
Martin Westerkamp <westerkamp@tu-berlin.de>
2019-12-19 16:36:04 +0100

    Adapt python script for validation

--- preprocessing/batch.py
+++ preprocessing/batch.py
@@ -5,10 +5,28 @@ from create_input import generateZokratesInputFromBlock
 
 cmd_compute_witness = 'zokrates compute-witness --light -a '
 cmd_generate_proof = 'zokrates generate-proof'
+BATCH_SIZE = 504
 
-
-for i in range(20,21):
-    os.system(cmd_compute_witness + generateZokratesInputFromBlock((i-1)*504+1, 504))
+def validateBatchFromBlockNo(blockNo):
+    os.system(cmd_compute_witness + generateZokratesInputFromBlock((blockNo-1)*BATCH_SIZE+1, BATCH_SIZE))
     os.system(cmd_generate_proof)
-    os.system('mv witness output/witness' + str(i))
-    os.system('mv proof.json output/proof' + str(i) + '.json')
+    os.system('mv witness output/witness' + str(blockNo))
+    os.system('mv proof.json output/proof' + str(blockNo) + '.json')
+
+def validateBatchesFromBlockNo(blockNo, amountBatches):
+    for i in range(0,amountBatches):
+        validateBatchFromBlockNo(blockNo+i*BATCH_SIZE)
+
+def main():
+    arguments = len(sys.argv) - 1
+    if arguments > 2:
+        print('Too many arguments. Arg. 1: Starting Block number, Arg. 2(optional): Amount of batches')
+    if arguments < 1:
+        print('Missing argument. Arg. 1: Starting Block number, Arg. 2(optional): Amount of batches') 
+    if arguments == 1:
+        validateBatchFromBlockNo(int(sys.argv[1]))
+    if arguments == 2:
+        validateBatchesFromBlockNo(int(sys.argv[1]),int(sys.argv[2]))
+
+if __name__ == "__main__":
+    main()
