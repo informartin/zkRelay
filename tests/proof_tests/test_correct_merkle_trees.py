5359884599e68e62d0119297f9f720e68a1f320c
Leonard Stutzer <leonard.stutzer@campus.tu-berlin.de>
2020-10-16 12:08:15 +0200

    add test for correct merkle proof batch size 5

--- tests/proof_tests/test_correct_merkle_trees.py
+++ tests/proof_tests/test_correct_merkle_trees.py
@@ -10,23 +10,16 @@ from preprocessing.create_input import generateZokratesInputForMerkleProof
 import os
 from tests import test_helper
 
-
-BATCH_SIZE=2
-
 # Mock Click.Context object that is normally passed through cli to functions 
 class Context:
     obj = {}
 
 class TestProofCorrectBlocks(unittest.TestCase):
+    verbose = False
 
     def setUp(self):
         self.ctx = Context()
         self.ctx.obj = toml.load('./tests/conf/local_conf.toml')
-
-        print('\nSetting up merkle proof environment...\n')
-        subprocess.run(['zkRelay', 'setup-merkle-proof'],
-                    check=True, stdout=subprocess.DEVNULL)
-        print('\nDone.')
 
     def test_batch_size_2(self):
         """
@@ -35,11 +28,12 @@ class TestProofCorrectBlocks(unittest.TestCase):
         batch_size = 2
         batch_no = 1
         first_block_in_batch = test_helper.get_first_block_in_batch(batch_no, batch_size)
+        block_range = test_helper.get_first_block_in_batch(batch_no + 1, batch_size)
 
         # check if required files are generated 
-        test_helper.setup_test_environment(batch_size, batch_no)
+        test_helper.setup_merkle_proof_test_environment(batch_size, batch_no, self.verbose)
 
-        for block_nr in range(first_block_in_batch, (batch_no * batch_size) + 1):
+        for block_nr in range(first_block_in_batch, block_range):
             counter = (block_nr - 1) % batch_size
             
             test_helper.exec_proof(self.ctx, '/test_proof/test_correct_proofs/batch_size_2_nr_{}.json'.format(counter), block_nr)
@@ -51,4 +45,30 @@ class TestProofCorrectBlocks(unittest.TestCase):
                         'inclusion prove for block nr {} in merkle tree of {} blocks was not successful.'.format(block_nr, batch_size))
                 
                 self.assertEqual(lines[1].rstrip(), '~out_2 11010401775869175601781796382403673205', 
+                        'inclusion prove for block nr {} in merkle tree of {} blocks was not successful.'.format(block_nr, batch_size))
+
+    def test_batch_size_5(self):
+        """
+            testing all blocks of batch size 5
+        """
+        batch_size = 5
+        batch_no = 1
+        first_block_in_batch = test_helper.get_first_block_in_batch(batch_no, batch_size)
+        block_range = test_helper.get_first_block_in_batch(batch_no + 1, batch_size)
+
+        # check if required files are generated 
+        test_helper.setup_merkle_proof_test_environment(batch_size, batch_no, self.verbose)
+
+        for block_nr in range(first_block_in_batch, block_range):
+            counter = (block_nr - 1) % batch_size
+            
+            test_helper.exec_proof(self.ctx, '/test_proof/test_correct_proofs/batch_size_5_nr_{}.json'.format(counter), block_nr)
+            
+            with open('./mk_tree_validation/witness', 'r') as witness:
+                lines = witness.readlines()
+
+                self.assertEqual(lines[0].rstrip(), '~out_3 31499779437727666674347312136552582663', 
+                        'inclusion prove for block nr {} in merkle tree of {} blocks was not successful.'.format(block_nr, batch_size))
+                
+                self.assertEqual(lines[1].rstrip(), '~out_2 30029047865384282741416230607321619582', 
                         'inclusion prove for block nr {} in merkle tree of {} blocks was not successful.'.format(block_nr, batch_size))
